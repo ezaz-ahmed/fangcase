f0292381e35a66c7a6d8c58efbb18eec621cae16
Ezaz Ahmed <ezaznahmed@gmail.com>
2022-08-12 12:36:22 +0600

    Baisc nextjs project setup with typescript

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import Head from 'next/head'
+import { FC } from 'react'
+
+const HomePage: FC = () => {
+  return (
+    <>
+      <Head>
+        <title>Next Shop</title>
+      </Head>
+
+      <main>
+        <h1>
+          Next Shop
+        </h1>
+      </main>
+    </>
+  )
+}
+
+export default HomePage
